4766f395cd275a86688e9f8ca4924fb7e6931353
LucasCrespo75 <65462062+LucasCrespo75@users.noreply.github.com>
2022-10-28 00:44:37 -0300

    Update App.java

--- livrariaAplicacao/src/controle/App.java
+++ livrariaAplicacao/src/controle/App.java
@@ -10,11 +10,11 @@ public class App {
 		
 		Biblioteca bibli = new Biblioteca();
 		bibli.getNome();
-		//adicionando um livro partindo do metodo String titulo, String autor
+		//adicionando um livro partindo do metodo Livro Livro
 		Livro livro1 = new Livro("Douglas o menino morto", "Tynah Guedes");
 		bibli.adicionarLivro(livro1);
 		
-		//adicionando um livro partindo o metodo Livro livro
+		//adicionando um livro partindo o metodo String titulo, String autor
 		bibli.adicionarLivro("AUTO DA COMPADECIDA", "LUCAS CRESPO");
 		System.out.println("Titulo: "+bibli.obterLivroTitu("AUTO DA COMPADECIDA" ).getTitulo());
 		System.out.println("Autor: "+bibli.obterLivroAut("LUCAS CRESPO").getAutor());
