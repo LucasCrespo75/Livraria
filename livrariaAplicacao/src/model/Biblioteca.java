feeccd6ba1872e7fb54e2d8f5fb02cc20a3c3d06
LucasCrespo75 <65462062+LucasCrespo75@users.noreply.github.com>
2022-12-15 18:45:22 -0300

    Update Biblioteca.java
    Correção efetuada

--- livrariaAplicacao/src/model/Biblioteca.java
+++ livrariaAplicacao/src/model/Biblioteca.java
@@ -2,7 +2,7 @@ package model;
 
 import java.util.ArrayList;
 
-public class Biblioteca extends Livro {
+public class Biblioteca {
 	
 	private String nome;
 	//private String local;
@@ -20,64 +20,9 @@ public class Biblioteca extends Livro {
 		this.nome = nome;
 	}
 	
-	public void adicionarLivro(String titulo, String autor) {
-       // Biblioteca biblio = new Biblioteca();
-
-		//livro = new Livro();
-		//livro.getCodigo();
-		//livro.setTitulo(livro.getTitulo());
-		//livro.setAutor(livro.getAutor());
-		//livro.getNumPag();
-		//livro.getValorCompra();
-		
-		Livro livro = new Livro(titulo, autor);
-		
-		listaLivros.add(livro);
-		
-	}
-	public void adicionarLivro(Livro livro) {
-		listaLivros.add(livro);
-		
-			
-	}
-
-	public Livro obterLivroTitu(String titulo) {
-		for(int i = 0; i < listaLivros.size(); i++) {
-			if(listaLivros.get(i).getTitulo().equals(titulo)) {
-				return listaLivros.get(i);
-			}
-		}
-		
-		return null;
-	}
-	
-	public Livro obterLivroAut(String autor) {
-		for(int i = 0; i < listaLivros.size(); i++) {
-			if(listaLivros.get(i).getAutor().equals(autor)) {
-				return listaLivros.get(i);
-			}
-		}
-		
-		return null;
-	}
-	
-	public void retornarTudo() {
-		for(int i = 0; i < listaLivros.size(); i++) {
-			System.out.println("Livro "+(i+1) + " - "+listaLivros.get(i).getTitulo());
-		}
-	}
-	
-	public void removerLivro(String titulo) {
-		for(int i = 0; i < listaLivros.size(); i++) {
-			if(listaLivros.get(i).getTitulo().equals(titulo)) {
-				listaLivros.remove(i);
-
-			}
-		}
-	}
 	
 	public String getNome() {
-		System.out.println("Bem vindo à SARAIVA!!");
+		System.out.println("Bem vindo Ã  SARAIVA!!");
 		return nome;
 	}
 	public void setNome(String nome) {
